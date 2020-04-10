578f95f2104525e2b2a9d9ed6fea4a71117de193
Roger Chui <chui101@gmail.com>
2020-04-09 21:55:41 -0400

    Fix bugs in changed parameters

--- calculate_trends.py
+++ calculate_trends.py
@@ -67,12 +67,10 @@ if __name__ == "__main__":
     for state in states:
         data = state_historic_data(state)
         latest_data = data.get_latest_n(7)
-        fit = death_growth_rate(latest_data)
-        print(state + "," + str(doubling_time(fit)))
+        print(state + "," + str(doubling_time(latest_data,'death')))
 
     print("Average case doubling time in days since 10th confirmed case")
     for state in states:
         data = state_historic_data(state)
         latest_data = data.get_after_n_cases(10)
-        fit = growth_rate(latest_data)
-        print(state + "," + str(doubling_time(fit)))
+        print(state + "," + str(doubling_time(latest_data)))
